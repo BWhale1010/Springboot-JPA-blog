b44e4ed1133dc3cd42bf7ccf2ba1346bf6d3ca66
bawool <bawool1010@gmail.com>
2024-03-14 18:09:54 +0900

    1. username unique 설정

--- src/main/java/com/bw/blog/test/DummyControllerTest.java
+++ src/main/java/com/bw/blog/test/DummyControllerTest.java
@@ -114,12 +114,12 @@ public class DummyControllerTest {
 
 	// http://localhost:8000/blog/dummy/join(요청)
 	// http의 body에 username, password, email 데이터를 가지고 요청
-	@PostMapping("/dummy/join")
+	
 //	public String join(String username, String password, String email) { // key = value (약속된 규칙)
 //		System.out.println("username : " + username);
 //		System.out.println("password : " + password);
 //		System.out.println("email : " + email);
-
+	@PostMapping("/dummy/join")
 	public String join(User user) {
 		System.out.println("id : " + user.getId());
 		System.out.println("username : " + user.getUsername());
